2c8d579707d7d97bc3053756e051f908f35e9359
Thomas Thron <tt@brandenburg-labs.com>
2020-09-02 14:54:17 +0200

    require python 3.8

--- setup.py
+++ setup.py
@@ -55,12 +55,8 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License'
     ],
-
+    python_requires='>=3.8'
 )
